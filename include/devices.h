9b1849a0234f9d42e04918b6b857b1447082f411
Sven <svenarne.lindstroem@gmail.com>
2026-02-08 13:47:16 +0100

    single purpose device implementation

--- include/devices.h
+++ include/devices.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "device.h"
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
 
 class SensorDevice: public Device{
   public:
@@ -23,6 +25,31 @@ class ReciverDevice: public Device{
     void (*process)(const char* msg, int pin);
   public:
     ReciverDevice( int pin, const char* type, void(*proc)(const char*, int));
+    void handleMessage(const char* msg) override;
+    void init() override;
+};
+
+class FanDevice: public Device {
+    int pin2;
+  public:
+    FanDevice( int pin1, int pin2);
+    void handleMessage(const char* msg) override;
+    void init() override;
+};
+
+class ServoDevice: public Device {
+    Servo servo;
+    int pos;
+  public:
+    ServoDevice( int pin1);
+    void handleMessage(const char* msg) override;
+    void init() override;
+};
+
+class LcdDevice: public Device {
+    LiquidCrystal_I2C mylcd;
+  public:
+    LcdDevice(int pin);
     void handleMessage(const char* msg) override;
     void init() override;
 };
--- src/devices.cpp
+++ src/devices.cpp
@@ -34,3 +34,66 @@ void ReciverDevice::init(){
 void ReciverDevice::handleMessage(const char* msg){
   process(msg, pin);
 }
+
+// fan class 
+FanDevice::FanDevice( int pin1, int pin2)
+: Device("fan", pin1, 2), pin2(pin2){}
+
+void FanDevice::init(){
+  Device::init();
+  pinMode(pin, OUTPUT);
+  pinMode(pin2, OUTPUT);
+}
+
+void FanDevice::handleMessage(const char *msg){
+  if(strcmp(msg, "0") == 0){
+    digitalWrite(pin, LOW);
+    digitalWrite(pin2, LOW);
+  }else if (strcmp(msg, "1") == 0){
+    digitalWrite(pin, HIGH);
+    digitalWrite(pin2, LOW);
+  }else if (strcmp(msg, "2") == 0){
+    digitalWrite(pin, LOW);
+    digitalWrite(pin2, HIGH);
+  }
+}
+
+// servo class
+ServoDevice::ServoDevice(int pin)
+: Device("servo", pin, 1),pos(0){}
+
+void ServoDevice::init(){ 
+  Device::init();
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  servo.attach(pin);
+}
+
+void ServoDevice::handleMessage(const char *msg){
+  if(strcmp(msg, "open") == 0){
+      servo.write(180);
+  }else if (strcmp(msg, "close") == 0){
+      servo.write(0);
+  }
+}
+
+// lcd screen class
+LcdDevice::LcdDevice(int pin)
+: Device("display", pin, 32), mylcd(LiquidCrystal_I2C(0x27,16,2)){}
+
+void LcdDevice::init(){ 
+  Device::init();
+  mylcd.init();
+  mylcd.backlight();
+}
+
+void LcdDevice::handleMessage(const char *msg){
+  mylcd.clear();
+  mylcd.setCursor(0,0);
+  for(int i = 0; msg[i] != '\0'; i++){
+    if (i == 16){
+      mylcd.setCursor(0, 1);
+    }
+    mylcd.print(msg[i]);
+  }
+}
