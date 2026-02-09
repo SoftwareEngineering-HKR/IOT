0a1fb1db943328eaccb4c20fecf16e721bb89513
Sven <svenarne.lindstroem@gmail.com>
2026-02-09 10:33:44 +0100

    device handler functions

--- src/device_service.cpp
+++ src/device_service.cpp
@@ -2,6 +2,28 @@
 #include "devices.h"
 #include <Arduino.h>
 
+void playMusic(const char* msg, int pin) {
+  for (int i = 0; msg[i] != '\0'; i++){
+    for(int j = 0; j < msg[i]; j++){
+      digitalWrite(pin, HIGH);
+      delay(2);
+      digitalWrite(pin, LOW);
+      delay(2);
+    }
+    delay(100);
+  }
+};
+
+void binaryReciver(const char* msg, int pin){
+  if(strcmp(msg, "on") == 0){
+    digitalWrite(pin, HIGH);
+  }else if (strcmp(msg, "off") == 0){
+    digitalWrite(pin, LOW);
+  }
+}
+
+
+
 void initDevices(){
 } 
 
