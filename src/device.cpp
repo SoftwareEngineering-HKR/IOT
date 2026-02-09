5238017425bc14b8848d027e378bef9fcc445c6b
Sven <svenarne.lindstroem@gmail.com>
2026-02-09 10:24:00 +0100

    topic construction

--- src/device.cpp
+++ src/device.cpp
@@ -9,6 +9,22 @@ namespace{
       mac[i] = random(0, 256);
     }
   }
+
+  void macToString(uint8_t mac[6], char buffer[13]){
+    int j = 0;
+    for(int i = 0; i < 6; i ++){
+      uint8_t f = (mac[i] & 0xF0) >> 4; 
+      uint8_t l = mac[i] & 0x0F; 
+
+      if(f <= 9){ buffer[j++] =  '0' + f;}
+      else{ buffer[j++] =  'A' + ( f - 10);}
+
+      if( l <= 9){ buffer[j++] =  '0' + l;}
+      else{ buffer[j++] = 'A' + ( l - 10);}
+    }
+    buffer[j] = '\0';
+  }
+
 }
 
 Device::Device(const char* type, int pin, int maxVal){
@@ -19,6 +35,9 @@ Device::Device(const char* type, int pin, int maxVal){
 }
 
 void Device::getTopic(char* buffer, size_t size){
+  char macBuffer[13];
+  macToString(mac, macBuffer);
+  snprintf(buffer, size, "%s/%s", type, macBuffer);
 }
 
 void Device::init(){
