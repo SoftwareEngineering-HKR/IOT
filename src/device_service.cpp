df217b0921ae0edac673e993fc284c708be156f0
Sven <svenarne.lindstroem@gmail.com>
2026-02-09 10:32:26 +0100

    bulk device handling interface

---
+++ src/device_service.cpp
@@ -0,0 +1,15 @@
+#include "device_service.h"
+#include "devices.h"
+#include <Arduino.h>
+
+void initDevices(){
+} 
+
+void handleMessage(char message[], String topic){
+}
+
+void makeDataReadings(MqttClient mqttClient){
+}
+
+void registerDevices(MqttClient mqttClient){
+}
