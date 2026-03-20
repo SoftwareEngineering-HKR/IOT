a55e7f71240d668594a81136263c982dd2e9c8b9
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-03-20 20:36:20 +0100

    Add humidity class

--- environmental_monitor/pico_devices.h
+++ environmental_monitor/pico_devices.h
@@ -8,7 +8,7 @@ extern DHT dhtSensor;
 // Device classes
 class DHTTempDevice : public Device {
 public:
-  DHTTempDevice(int pin) : Device("temperature", pin, 50) {}
+  DHTTempDevice(int pin) : Device("temperature", pin, 50) {} // DHT11 max temp is 50C
   
   void init() override {
     WiFi.macAddress(this->mac); // Inject  Pico W Wi-Fi MAC
@@ -17,5 +17,18 @@ public:
   int getReading() override {
     float t = dhtSensor.readTemperature();
     return isnan(t) ? -1 : (int)t;
+  }
+};
+class DHTHumDevice : public Device {
+public:
+  DHTHumDevice(int pin) : Device("humidity", pin, 100) {} // DHT11 max humidity is 100%
+  
+  void init() override {
+    WiFi.macAddress(this->mac); 
+  }
+  
+  int getReading() override {
+    float h = dhtSensor.readHumidity();
+    return isnan(h) ? -1 : (int)h;
   }
 };
