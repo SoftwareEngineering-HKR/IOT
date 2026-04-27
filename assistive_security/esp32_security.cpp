b6a34c53b70017db9b7f4df4bcbfd8e7d89813ec
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2026-04-27 10:30:32 +0200

    initialize network and timing variables

--- assistive_security/esp32_security.cpp
+++ assistive_security/esp32_security.cpp
@@ -18,3 +18,12 @@ WiFiUDP udp;
 IPAddress serverIP;
 bool serverFound = false;
 
+// -----------------------------------------------------------------------------
+// Timing
+// -----------------------------------------------------------------------------
+static const unsigned long UPDATE_INTERVAL_MS = 200UL;
+static const unsigned long RFID_UNLOCK_TIME_MS = 10000UL;
+
+unsigned long lastUpdateAt = 0;
+unsigned long lastWifiAttemptAt = 0;
+unsigned long lastMqttAttemptAt = 0;
