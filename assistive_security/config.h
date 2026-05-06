14754d9317d226c5cdb0c6ee07b78d9fc2c9b9cd
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2026-05-06 13:08:18 +0200

    ESP32 base I2C pin mapping

--- assistive_security/config.h
+++ assistive_security/config.h
@@ -5,3 +5,21 @@
 // -----------------------------------------------------------------------------
 #define WIFI_SSID "*****"
 #define WIFI_PASSWORD "*******"
+
+
+// -----------------------------------------------------------------------------
+// ESP32-S3 Super Mini hardware mapping
+// RC522 and VL53L0X share the same I2C bus
+// -----------------------------------------------------------------------------
+#define BOARD_LED_PIN 8
+
+#define I2C_SDA_PIN 5
+#define I2C_SCL_PIN 6
+
+#define LIDAR_SDA_PIN I2C_SDA_PIN
+#define LIDAR_SCL_PIN I2C_SCL_PIN
+
+
+#define RC522_SDA_PIN I2C_SDA_PIN
+#define RC522_SCL_PIN I2C_SCL_PIN
+#define RC522_I2C_ADDRESS 0x28
