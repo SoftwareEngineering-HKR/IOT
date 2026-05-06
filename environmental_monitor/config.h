e1b084414006d03338bb406f96c595aa2c80f873
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2026-05-06 13:56:53 +0200

    copy code from omer br to main

--- environmental_monitor/config.h
+++ environmental_monitor/config.h
@@ -2,9 +2,11 @@
 #define CONFIG_H
 
 // Wi-Fi credentials
-#define WIFI_SSID "*******"
-#define WIFI_PASSWORD "******"
+#define WIFI_SSID "Emperor"
+#define WIFI_PASSWORD ""
 
+// Discord Webhook
+#define DISCORD_WEBHOOK_URL "https://discord.com/api/webhooks/*****"
 
 // Thresholds
 #define TEMP_THRESHOLD 30
