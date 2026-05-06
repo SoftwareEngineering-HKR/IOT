84672dddd9e515265c167e091abffaf7c0fd3f53
MuhammadOmerQureshi <omer.qureshi0001@stud.hkr.se>
2026-05-06 12:51:23 +0200

    add MAC derivation and LidarDevice implementation

--- assistive_security/esp32_devices.h
+++ assistive_security/esp32_devices.h
@@ -54,4 +54,59 @@ public:
 
   virtual void init() = 0;
 
+
+  
+
+private:
+  static void macToString(const uint8_t inMac[6], char buffer[13]) {
+    static const char hex[] = "0123456789ABCDEF";
+    int j = 0;
+
+    for (int i = 0; i < 6; i++) {
+      buffer[j++] = hex[(inMac[i] >> 4) & 0x0F];
+      buffer[j++] = hex[inMac[i] & 0x0F];
+    }
+
+    buffer[j] = '\0';
+  }
+};
+
+// -----------------------------------------------------------------------------
+// Shared hardware objects
+// -----------------------------------------------------------------------------
+#define C3_LED_PIN BOARD_LED_PIN
+
+VL53L0X lidar;
+MFRC522 rfid(RC522_I2C_ADDRESS, RC522_RST_PIN);
+
+inline void assignDerivedMac(uint8_t outMac[6], uint8_t offset) {
+  WiFi.macAddress(outMac);
+  outMac[5] = (uint8_t)(outMac[5] + offset);
+  if (outMac[5] == 0x00) {
+    outMac[5] = offset;
+  }
+}
+
+// -----------------------------------------------------------------------------
+// LIDAR VL53L0X
+// -----------------------------------------------------------------------------
+class LidarDevice : public SecurityDevice {
+public:
+  explicit LidarDevice(int lidarPin)
+    : SecurityDevice("photo", lidarPin, LIDAR_MAX_VALUE) {}
+
+  void init() override {
+    assignDerivedMac(this->mac, 1);
+
+    lidar.setTimeout(500);
+
+    if (!lidar.init()) {
+      Serial.println("[lidar] Failed to detect and initialize VL53L0X");
+      return;
+    }
+
+    lidar.startContinuous();
+    Serial.println("[lidar] VL53L0X ready");
+  }
+
   
