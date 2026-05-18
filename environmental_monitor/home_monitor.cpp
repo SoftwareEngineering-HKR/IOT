8558f14e91491112a76b74b1d8a78a1579b28720
EmperorOlashinbo <ib.olasunbo@gmail.com>
2026-05-18 14:07:41 +0200

    Removed discord emergency alert

--- environmental_monitor/home_monitor.cpp
+++ environmental_monitor/home_monitor.cpp
@@ -2,38 +2,28 @@
 #include <WiFiUdp.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoMqttClient.h>
-#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
 #include "config.h" 
-#include "pico_devices.h" 
 
 
 #define DHT_TYPE DHT11
 DHT dhtSensor(DHT_PIN, DHT_TYPE);
 
-// MQTT
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 WiFiUDP udp;
 
-// Server discovery
-const int UDP_PORT = 1883; 
+const int UDP_PORT = 4444; 
 IPAddress serverIP;
 bool serverFound = false;
 
-// Update interval in milliseconds
-const unsigned long UPDATE_INTERVAL = 60000; // 60s
+const unsigned long UPDATE_INTERVAL = 60000; 
 unsigned long lastUpdate = 0;
 
-// Device instances
-DHTTempDevice tempDev(DHT_PIN);
-DHTHumDevice  humDev(DHT_PIN);
-LightDevice   lightDev(ADC_PIN);
-TiltDevice    tiltDev(TILT_PIN);
+// Base MAC String
+String baseMac;
 
-// Array of device pointers for easy iteration
-Device* myDevices[] = {&tempDev, &humDev, &lightDev, &tiltDev};
-const int deviceCount = sizeof(myDevices) / sizeof(myDevices[0]);
 
 void connectWiFi() {
   Serial.print("Connecting to Wi-Fi");
@@ -43,6 +33,13 @@ void connectWiFi() {
     delay(1000);
   }
   Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
+  
+  byte mac[6];
+  WiFi.macAddress(mac);
+  for (int i = 0; i < 6; ++i) {
+    baseMac += String(mac[i], HEX);
+  }
+  baseMac.toUpperCase();
 }
 
 void discoverServer() {
@@ -51,7 +48,7 @@ void discoverServer() {
   
   while (!serverFound) {
     udp.beginPacket(IPAddress(255, 255, 255, 255), UDP_PORT);
-    udp.print("DISCOVER_SERVER"); 
+    udp.print("NetworkDiscovery;Ver=1;"); 
     udp.endPacket();
     
     delay(2000);
@@ -64,70 +61,66 @@ void discoverServer() {
   }
   udp.stop();
 }
+
+void registerSensor(String suffix, String type, float minVal, float maxVal) {
+  JsonDocument doc;
+  String uniqueId = baseMac + "-" + suffix;
+  
+  doc["id"] = uniqueId;
+  doc["type"] = type;
+  doc["minVal"] = minVal;
+  doc["maxVal"] = maxVal;
+  doc["sensor"] = true;
+  
+  char buffer[256];
+  serializeJson(doc, buffer);
+  
+  mqttClient.beginMessage("register");
+  mqttClient.print(buffer);
+  mqttClient.endMessage();
+  Serial.println("Registered: " + String(buffer));
+}
+
 void connectMQTTAndRegister() {
   Serial.print("Connecting to MQTT broker...");
-  if (!mqttClient.connect(serverIP, 1883)) {
+  if (!mqttClient.connect("192.168.1.2", 1883)) {
     Serial.println("Failed! Error code = " + String(mqttClient.connectError()));
     return;
   }
   Serial.println("Connected!");
 
-  // Use the `getRegistration` method from the Device class
-  char buffer[128];
-  for (int i = 0; i < deviceCount; i++) {
-    myDevices[i]->getRegistration(buffer);
-    
-    mqttClient.beginMessage("registration");
-    mqttClient.print(buffer);
-    mqttClient.endMessage();
-  }
+  // Register each sensor uniquely
+  registerSensor("TMP", "temperature", -10, 50);
+  registerSensor("HUM", "humidity", 0, 100);
+  registerSensor("LGT", "brightness", 0, 100);
+  registerSensor("TLT", "tilt_status", 0, 1);
 }
-void checkAndAlertDiscord(int t, int h, int l, int tilt) {
-  if (WiFi.status() != WL_CONNECTED) return;
 
-  bool tempAlert  = (t != -1 && t > TEMP_THRESHOLD);
-  bool humAlert   = (h != -1 && h > HUMIDITY_THRESHOLD);
-  bool lightAlert = (l != -1 && l < LIGHT_THRESHOLD);
-  bool tiltAlert  = (tilt == 1);
-
-  if (!tempAlert && !humAlert && !lightAlert && !tiltAlert) return;
-
-  String msg = "EMERGENCY ALERT! Temp: " + (t == -1 ? String("N/A") : String(t) + "C") + 
-               ", Hum: " + (h == -1 ? String("N/A") : String(h) + "%") + 
-               ", Light: " + String(l) + "%, Tilt: " + (tilt ? "Tilted" : "Safe");
-
-  String payload = "{\"content\": \"" + msg + "\"}";
-
-  WiFiClientSecure client;
-  client.setInsecure(); 
-  HTTPClient http;
-  
-  http.begin(client, DISCORD_WEBHOOK_URL);
-  http.addHeader("Content-Type", "application/json");
-  
-  int code = http.POST(payload);
-  if (code > 0 && code / 100 == 2) Serial.println("Emergency alert sent to Discord!");
-  http.end();
+void publishReading(String suffix, String type, float value) {
+  String topic = type + "/" + baseMac + "-" + suffix;
+  mqttClient.beginMessage(topic);
+  mqttClient.print(value);
+  mqttClient.endMessage();
+  Serial.println("Published to " + topic + ": " + String(value));
 }
+
 void setup() {
   Serial.begin(115200);
   delay(2000);
+  Serial.println("Pico W Env Monitor Starting ...");
 
   pinMode(TILT_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
-  analogReadResolution(16);
+  // Set ADC resolution to 12 bits (0-4095) for light sensor readings
+  analogReadResolution(12);
   
   dhtSensor.begin();
-
-  // Initialize all devices
-  for (int i = 0; i < deviceCount; i++) {
-    myDevices[i]->init();
-  }
 
   connectWiFi();
   discoverServer();
   connectMQTTAndRegister();
 }
+
 void loop() {
   if (serverFound && !mqttClient.connected()) {
     connectMQTTAndRegister();
@@ -140,28 +133,19 @@ void loop() {
   if (now - lastUpdate >= UPDATE_INTERVAL) {
     lastUpdate = now;
 
-    for (int i = 0; i < deviceCount; i++) {
-      int reading = myDevices[i]->getReading();
-      
-      if (reading != -1) {
-        char topicBuf[64];
-        myDevices[i]->getTopic(topicBuf, sizeof(topicBuf));
-        
-        mqttClient.beginMessage(topicBuf);
-        mqttClient.print(reading);
-        mqttClient.endMessage();
-        
-        Serial.println("Published to " + String(topicBuf) + ": " + String(reading));
-      }
-    }
-
-    // Capture variables for Discord and LED check
-    int t    = tempDev.getReading();
-    int h    = humDev.getReading();
-    int l    = lightDev.getReading();
-    int tilt = tiltDev.getReading();
-
+    // Read Sensors
+    float t = dhtSensor.readTemperature();
+    float h = dhtSensor.readHumidity();
+    int rawLight = analogRead(ADC_PIN);
+    float l = ((float)rawLight / 4095.0) * 100.0;
+    int tilt = (digitalRead(TILT_PIN) == LOW) ? 1 : 0;
     digitalWrite(LED_PIN, tilt == 1 ? HIGH : LOW);
-    checkAndAlertDiscord(t, h, l, tilt);
+
+    // Publish valid readings
+    if (!isnan(t)) publishReading("TMP", "temperature", t);
+    if (!isnan(h)) publishReading("HUM", "humidity", h);
+    publishReading("LGT", "brightness", l);
+    publishReading("TLT", "tilt_status", tilt);
+    
   }
 }
